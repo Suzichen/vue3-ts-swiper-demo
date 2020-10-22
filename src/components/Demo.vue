5cc9787b18acf798d9befff0aba15a157ccabb6b
suzichen <suzichenapk@outlook.com>
2020-10-22 16:31:52 +0800

    update: 使用Hooks管理数据

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -4,10 +4,10 @@
       <template #default>
         <swiper-item :no-swiping="true">
           <span style="color: red">Vertical Slide 1 as Vue components</span>
-          <button @click="test">更新下一页数据并跳转</button>
+          <button @click="doAdd(15)">加15条数据并跳转</button>
         </swiper-item>
-        <swiper-item ref="scrollSwiperRef" :auto-scroll="true" v-if="plength">
-          <p v-for="i in plength" :key="i">{{ i }}: 动态长度页面</p>
+        <swiper-item ref="scrollSwiperRef" :auto-scroll="true" v-if="counter">
+          <p v-for="i in counter" :key="i">{{ i }}: 动态长度页面</p>
         </swiper-item>
         <swiper-item>
           <span style="color: red">Vertical Slide 3 as Vue components</span>
@@ -20,6 +20,24 @@
 <script lang="ts">
 import { defineComponent, nextTick, ref } from 'vue'
 import { Swiper, SwiperItem } from './Swiper'
+
+const useCounter = () => {
+  const counter = ref(0)
+  const addLen = ref(0)
+  const increment = (n: number) => {
+    addLen.value += n
+    counter.value = addLen.value
+  }
+  const reset = () => {
+    counter.value = 0
+  }
+  return {
+    counter,
+    increment,
+    reset
+  }
+}
+
 export default defineComponent({
   name: 'Demo',
   components: {
@@ -27,40 +45,37 @@ export default defineComponent({
     SwiperItem
   },
   setup () {
-    const plength = ref(0)
-    const addLen = ref(0)
+    const { counter, increment, reset } = useCounter()
     const swiperRef = ref({} as typeof Swiper)
     const scrollSwiperRef = ref({} as typeof SwiperItem)
-    // 更新动态页组件并跳转
+    // 更新组件并跳转
     const updateSwiperItem = () => {
       swiperRef.value.updateSlides()
       scrollSwiperRef.value.update()
       swiperRef.value.next()
     }
     // 更新数据
-    const test = () => {
-      addLen.value += 15
-      plength.value = addLen.value
+    const doAdd = (n: number) => {
+      increment(n)
       nextTick(() => { updateSwiperItem() })
     }
     // 记录索引值
     const swipeIndex = ref(0)
     const onSwipeChange = (i: number) => {
       swipeIndex.value = i
-      if (i === 0) plength.value = 0 // 当返回到动态高度页的上一页时，需要销毁此动态页以确保下次滚动不出错(此demo用v-if)
+      if (i === 0) reset() // 当返回到动态高度页的上一页时，需要销毁此动态页以确保下次滚动不出错(此demo用v-if)
     }
     // 跳转下一页
     const next = () => {
       swiperRef.value.next()
     }
     return {
-      plength,
-      addLen,
+      counter,
       swipeIndex,
       swiperRef,
       scrollSwiperRef,
       next,
-      test,
+      doAdd,
       onSwipeChange
     }
   }
